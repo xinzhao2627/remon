55b0b84ae92006da4b6e177160ebcb73467dcae9
Rainnand <xinzhao2627@gmail.com>
2025-03-12 21:47:32 +0800

    added analytics

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,33 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
-import RandomSentence from './RandomSentence'
-import {ChakraProvider} from '@chakra-ui/react'
-import theme from './theme'
-import { ColorModeScript } from '@chakra-ui/react'
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import Quiz from './Quiz'
-import Custom from './Custom'
-import CustomInput from './CustomInput'
-import Home from './Home'
-import ErrorPage from './ErrorPage'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import RandomSentence from "./RandomSentence";
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "./theme";
+import { ColorModeScript } from "@chakra-ui/react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Quiz from "./Quiz";
+import Custom from "./Custom";
+import CustomInput from "./CustomInput";
+import Home from "./Home";
+import ErrorPage from "./ErrorPage";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-  <ChakraProvider>
-    <Router>
-      <ColorModeScript initialColorMode={theme} />
-      <Routes>
-        <Route exact path = '/home' Component={Home}/>
-        <Route exact path = '/' Component={Home}/>
-        <Route exact path='/random' Component={RandomSentence}/>
-        <Route exact path='/quiz' Component={Quiz}/>
-        <Route exact path='/custom' Component={Custom}/>
-        <Route exact path='/custominput' Component={CustomInput}/>
-        <Route exact path = '*' Component={ErrorPage}/>
-      </Routes>
-    </Router>
-</ChakraProvider>
-
-  </React.StrictMode>,
-)
+    <ChakraProvider>
+      <Router>
+        <ColorModeScript initialColorMode={theme} />
+        <Routes>
+          <Route exact path="/home" Component={Home} />
+          <Route exact path="/" Component={Home} />
+          <Route exact path="/random" Component={RandomSentence} />
+          <Route exact path="/quiz" Component={Quiz} />
+          <Route exact path="/custom" Component={Custom} />
+          <Route exact path="/custominput" Component={CustomInput} />
+          <Route exact path="*" Component={ErrorPage} />
+        </Routes>
+        <Analytics />
+        <SpeedInsights />
+      </Router>
+    </ChakraProvider>
+  </React.StrictMode>
+);
